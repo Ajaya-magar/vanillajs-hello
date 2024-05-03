bdc7469423a06afdb2be31d85e30effa40373b08
Ajaya-magar <156473678+Ajaya-magar@users.noreply.github.com>
2024-05-03 22:25:05 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "raccoon"];
+let extension = [".com", ".pt", ".be", ".cat"];
+let Output = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function Name(value1, value2, value3, value4) {
+  for (let i = 0; i < value1.length; i++) {
+    for (let j = 0; j < value2.length; j++) {
+      for (let k = 0; k < value3.length; k++) {
+        for (let l = 0; l < value4.length; l++) {
+          let domainName = value1[i] + value2[j] + value3[k] + value4[l];
+          Output.push(domainName);
+          let myTarget = document.getElementById("hello");
+          let myElement = document.createElement("p");
+          myElement.innerText = domainName;
+          myTarget.appendChild(myElement);
+        }
+      }
+    }
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+Name(pronoun, noun, adj, extension);
+console.log(Output);
